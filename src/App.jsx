a64477968ba0b13a47fb45a4461d30a7b4277b8b
techpedroso <technologypedroso@gmail.com>
2026-08-22 22:01:47 -0300

    feat(app): add initial layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
 import { useState } from "react"; //importa o useState do React
 
-import "./App.css";
-
-import Container from "./components/Container/Container.jsx";
 import Task from "./components/Task/Task.jsx"
 import AddTask from "./components/AddTask/AddTask.jsx"
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação a noite",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: "Estudar matemática",
+      description: "Estudar matemática a noite",
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: "Estudar fisíca",
+      description: "Estudar fisíca a noite",
+      isCompleted: false
+    },
+  ])
+  const [theme, setTheme] = useState("dark");
 
   function toggleTheme() {
     setTheme(theme === "light" ? "dark" : "light");
   }
 
   return (
-    <div className="app" data-theme={theme}>
+    <div className="app flex align-center flex-column gap-lg" data-theme={theme}>
       {/* Header */}
-      <Container variant="medium">
-        <h1>Task Manager</h1>
+      <div className="flex gap-lg" variant="medium">
+        <h1 className="title">Task Manager</h1>
 
-        <button onClick={toggleTheme}>
-          Alterar tema
+        <button className="themeBtn" onClick={toggleTheme}>
+          Theme
         </button>
-      </Container>
+      </div>
 
       {/* Form */}
-      <Container variant="medium">
-
-      </Container>
+      <div variant="medium">
+    
+      </div>
 
       {/* List */}
-      <Container variant="medium">
-        
-      </Container>
+      <div className="" variant="medium">
+        <Task tasks={tasks}>
+        </Task>
+      </div>
     </div>
   );
 }
